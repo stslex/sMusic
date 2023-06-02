70fd9f3b0439d57c8bbde40fee70f3c80b590049
stslex <ilya977.077@gmail.com>
2023-06-02 08:49:33 +0300

    init build logic

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,46 +1,13 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.library)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("smusic.android.library")
     kotlin("plugin.serialization") version "1.8.10"
 }
 
-android {
-    namespace = "com.stslex.core.network"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-}
+android.namespace = "com.stslex.core.network"
 
 dependencies {
     implementation(libs.bundles.ktor)
     implementation(libs.bundles.okhttp)
     implementation(libs.bundles.koin)
-
-    implementation(libs.core.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -1,44 +1,10 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.library)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
-    kotlin("plugin.serialization") version "1.8.10"
+    id("smusic.android.library")
+    id("smusic.android.library.compose")
 }
 
-android {
-    namespace = "com.stslex.feature.search"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.2"
-    }
-}
+android.namespace = "com.stslex.feature.search"
 
 dependencies {
     implementation(project(":core:ui"))
@@ -47,9 +13,4 @@ dependencies {
     implementation(project(":core:player"))
 
     implementation(libs.bundles.koin)
-    implementation(libs.bundles.ktor)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
